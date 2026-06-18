370899684332e20bf16fb80618f3a205e134072f
EncryptedNoobi <williamlawson028@gmail.com>
2026-06-18 23:53:02 +0100

    v1.0.0 + mobile navbar

--- src/components/globals/navbar.tsx
+++ src/components/globals/navbar.tsx
@@ -1,16 +1,17 @@
-import { Button, Dropdown } from "@heroui/react"
+import { Button, Drawer, Dropdown } from "@heroui/react"
 import { useState } from "react"
-import { TbChevronUp } from "react-icons/tb"
+import { TbChevronUp, TbMenu } from "react-icons/tb"
 
 export const Navbar = () => {
 
     const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [mobileNavbarOpen, setMobileNavbarOpen] = useState(false);
 
     return (
         <>
-        
+
             <div className="z-5 relative w-full h-[90xp] bg-(--primary-colour) border-b border-b-[3px] border-(--secondary-accent-colour)">
-                <div className="flex flex-row items-center justify-between p-[10px_10%] h-[70px]">
+                <div className="z-5 relative bg-(--primary-colour) flex flex-row items-center justify-between p-[10px_10%] h-[70px]">
 
                     <div className="h-[70px] flex flex-row items-center justify-center select-none">
                         <a href="/" className="text-(--text-colour) text-[20px] font-semibold">
@@ -18,7 +19,8 @@ export const Navbar = () => {
                         </a>
                     </div>
 
-                    <div className="h-[70px] flex flex-row items-center justify-center gap-[10px] select-none">
+                    {/* Desktop Navbar */}
+                    <div className="phone:hidden h-[70px] flex flex-row items-center justify-center gap-[10px] select-none">
                         <a href="/">
                             <Button variant="secondary">
                                 Home
@@ -46,7 +48,7 @@ export const Navbar = () => {
                                     <Dropdown.Item href="/committee">
                                         Committee
                                     </Dropdown.Item>
-                                    
+
                                     <Dropdown.Item href="/association-rules">
                                         Association Rules
                                     </Dropdown.Item>
@@ -74,10 +76,106 @@ export const Navbar = () => {
                         </a>*/}
                     </div>
 
+                    {/* Mobile Navbar Button */}
+                    <div className="hidden phone:flex h-[70px] flex flex-row items-center justify-center gap-[10px] select-none">
+
+                        <Button
+                            onClick={() => setMobileNavbarOpen(!mobileNavbarOpen)}
+                            variant="secondary"
+                            isIconOnly>
+                            <TbMenu />
+                        </Button>
+
+                    </div>
+
                 </div>
+
             </div>
-        
+
+            <MobileNavbarDrawer mobileNavbarOpen={mobileNavbarOpen} setMobileNavbarOpen={setMobileNavbarOpen} dropdownOpen={dropdownOpen} setDropdownOpen={setDropdownOpen} />
+
         </>
     )
+
+}
+
+export const MobileNavbarDrawer = ({ mobileNavbarOpen, setMobileNavbarOpen }: any) => {
+
+        const [mobileDropdownOpen, setMobileDropdownOpen] = useState(false);
+
+    return (
+        <>
+
+            <Drawer isOpen={mobileNavbarOpen} onOpenChange={setMobileNavbarOpen}>
+                <Drawer.Backdrop>
+                    <Drawer.Content placement="bottom">
+                        <Drawer.Dialog>
+                            <Drawer.CloseTrigger />
+
+                            <div className={`flex flex-col items-stretch gap-[10px] p-[10px] ${mobileNavbarOpen ? "" : "h-0"}`}>
+
+                                <a href="/">
+                                    <Button variant="secondary" className="w-full">
+                                        Home
+                                    </Button>
+                                </a>
+
+                                <a href="/routes">
+                                    <Button variant="secondary" className="w-full">
+                                        Routes
+                                    </Button>
+                                </a>
+
+                                <Dropdown isOpen={mobileDropdownOpen} onOpenChange={setMobileDropdownOpen}>
+                                    <Button variant="secondary" className="w-full">
+                                        About Us
+                                        <TbChevronUp className={`transition-[rotate] duration-150 ${mobileDropdownOpen ? "rotate-[180deg]" : ""}`} />
+                                    </Button>
+                                    <Dropdown.Popover>
+                                        <Dropdown.Menu>
+
+                                            <Dropdown.Item href="/faqs">
+                                                FAQs
+                                            </Dropdown.Item>
+
+                                            <Dropdown.Item href="/committee">
+                                                Committee
+                                            </Dropdown.Item>
+
+                                            <Dropdown.Item href="/association-rules">
+                                                Association Rules
+                                            </Dropdown.Item>
+
+                                        </Dropdown.Menu>
+                                    </Dropdown.Popover>
+                                </Dropdown>
+
+                                <a href="/membership">
+                                    <Button variant="secondary" className="w-full">
+                                        Membership
+                                    </Button>
+                                </a>
+
+                                <a href="/contact-us">
+                                    <Button variant="secondary" className="w-full">
+                                        Contact Us
+                                    </Button>
+                                </a>
+
+                                {/*<a href="/other-info">
+                            <Button variant="secondary" className="w-full">
+                                Other Info
+                            </Button>
+                        </a>*/}
+
+                            </div>
+
+                        </Drawer.Dialog>
+                    </Drawer.Content>
+                </Drawer.Backdrop>
+            </Drawer>
+
+        </>
+    );
 
 }
